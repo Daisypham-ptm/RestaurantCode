9b69335364569405879288d50420f964f5cb842a
Daisypham <phamthimuoi1086@gmail.com>
2026-01-27 00:25:54 +0700

    Delete db.py

--- db.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-import os
-
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DB_PATH = os.path.join(BASE_DIR, "database", "restaurant_menu.db")
-
-def get_connection():
-    return sqlite3.connect(DB_PATH)
-
